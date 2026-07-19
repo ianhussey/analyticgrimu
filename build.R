d7d164ede13aca95cdb6c8f9d6d076b0788e9529
Ian Hussey <ianhusseysipod@gmail.com>
2026-07-19 12:54:02 +0200

    standardized build.R

--- build.R
+++ build.R
@@ -5,25 +5,24 @@ setwd("~/git/analyticgrimu")
 
 devtools::document()
 
-devtools::check(vignettes = FALSE)
+devtools::check()
 
-#devtools::install(build_vignettes = TRUE)
-#vignette("analyticgrimu")
+# once you have the package updated, you can use it to build the vignettes, check the whole thing, and reinstall again
+devtools::build_vignettes()
 
-# or from github, after push
-devtools::install_github("ianhussey/analyticgrimu")
+devtools::install(build_vignettes = TRUE)
+
+# # or from github, after push
+# devtools::install_github("ianhussey/analyticgrimu")
 
 library(analyticgrimu)
 
 ?analyticgrimu
-
 vignette("analyticgrimu")
+
 
 detach("package:analyticgrimu", unload = TRUE)
 
-# once you have the package updated, you can use it to build the vignettes, check the whole thing, and reinstall again
-devtools::build_vignettes()
-devtools::check()
 
 # cran checks
 # win-builder
@@ -32,5 +31,4 @@ check_win_devel() # emails results to the maintainer address in DESCRIPTION
 
 # R-hub
 library(rhub)
-# rhub_setup() # one time
 rhub_check() # v2: runs on GitHub Actions in your repo
